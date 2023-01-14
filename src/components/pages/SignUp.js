2afe0b1f23e572f1ac76626089f57bf2f166feb2
sid120 <siddharthrakshe12@gmail.com>
2023-01-14 22:48:10 +0530

    updated SignUp page

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,5 +1,44 @@
-import React from "react";
-import '../../App.css'
-export default function SignUp(){
-    return <h1 className="Sign-Up">SIGN UP</h1>
+import '../../App.css';
+import React, { useState } from "react";
+
+function SignUpPage() {
+  const [login, setLogin] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // add code to handle the form submission here,
+    // such as sending the login, username, and password to a server
+  };
+
+  return (
+    <form onSubmit={handleSubmit} >
+      <label>
+        Login
+      </label>
+      <br />
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
 }
+
+export default SignUpPage;
